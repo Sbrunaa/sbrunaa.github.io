158750788bdb7657884838485cb6236c0a0110b5
Bruna Santos <ssbru24@gmail.com>
2024-12-16 16:26:22 -0300

    quase feito

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,20 @@ function changeTheme(){
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const acordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+        accordionActive ? acordionItem.classList.remove("active") : acordionItem.classList.add("active");
+
+        accordionActive ? acordionItem.classList.remove("active")  : acordionItem.classList.add("active");
+    })
+})
+
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menulinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
